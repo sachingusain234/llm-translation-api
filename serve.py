3c75d24031682040552084c6f2b0ff3ccad2e5ab
sachingusain234 <142111360+sachingusain234@users.noreply.github.com>
2026-01-06 13:46:56 +0530

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+from langserve import add_routes
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+model = ChatGroq(model="moonshotai/kimi-k2-instruct",groq_api_key=groq_api_key)
+
+# Creating the Prompt Template 
+system_template = "Translate the following into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ('system',system_template),
+        ('user','{text}')
+    ]
+)
+
+# Creating the output parser
+parser = StrOutputParser()
+
+# Create Chain
+chain = prompt_template|model|parser
+
+# App definition
+app = FastAPI(title="Langchain Server",
+             version="1.0",
+              description="A simple API server using Langchain runnable interfaces"
+             )
+
+# Adding chain routes
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="127.0.0.1",port=8000)
+
